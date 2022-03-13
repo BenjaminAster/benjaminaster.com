585ba1b857d99d96a086a9840c01f3c27327f66d
Benjamin Aster <nimajneb0905@gmail.com>
2022-03-13 11:44:47 +0100

    1.0.0

---
+++ projects.js
@@ -0,0 +1,98 @@
+
+const /** @type {Record<string, string>[]} */ projects = [
+	{
+		url: "/geoquiz/",
+	},
+	{
+		url: "/editor/",
+	},
+	{
+		url: "/svg2image/",
+	},
+	{
+		url: "/notepad/",
+	},
+	{
+		url: "/watertyper/",
+	},
+	{
+		url: "/colors/",
+	},
+	{
+		url: "/clock/",
+	},
+	{
+		url: "/tiny-snake/",
+		image: "/tiny-snake/icon.svg",
+		title: "Tiny Snake",
+		description: "Probably the worlds smallest snake game in less than 400 characters of HTML & JavaScript.",
+	},
+	{
+		url: "/unicode/",
+	},
+	{
+		url: "/3d-css-calculator/",
+	},
+	{
+		url: "/memorize-pi-digits/",
+	},
+	{
+		url: "/browser-version/",
+	},
+	{
+		url: "https://ars.electronica.art/keplersgardens/en/robdiloclock/",
+		image: "https://ars.electronica.art/prix/files/2021/08/robdiloclock.jpg",
+		title: "Robdilo'clock",
+		description: "A robot that grasps individual segments and positions them in such a way that they display the time in digital numerals."
+	},
+	{
+		url: "/html-entities/",
+	},
+	{
+		url: "/copy-quizlet/",
+	},
+	{
+		url: "/yt-ad-to-link/",
+	},
+	{
+		url: "/2048/",
+	},
+	{
+		url: "/game-of-life/",
+	},
+	{
+		url: "https://ars.electronica.art/outofthebox/en/plottegoino/",
+		image: "https://ars.electronica.art/outofthebox/files/2019/08/plotter@benjaminaster.jpg",
+		title: "Plottegoino",
+		description: "An Arduino-controlled Lego plotter that can print any desired letters."
+	},
+	{
+		url: "/mandelbrot/",
+	},
+	{
+		url: "/translate/",
+	},
+	{
+		url: "/tetris/",
+	},
+	{
+		url: "/domino-builder/",
+	},
+	{
+		url: "/pascal-triangle/",
+	},
+	{
+		url: "/take-away-triangles/",
+	},
+	{
+		url: "/anaglyph-cube/",
+	},
+	{
+		url: "/self-loading-html/",
+	},
+	{
+		url: "/unblock-me/",
+	},
+];
+
+export default projects;
